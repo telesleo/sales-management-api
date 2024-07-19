b1b43ee174fd527daa1dabd440e917d1ed383951
leo <leonardoandrei41@gmail.com>
2024-07-19 17:28:28 -0300

    Add create_customers_table migration

---
+++ database/migrations/1721420707068_create_customers_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'customers'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('cpf', 11).unique().notNullable()
+      table.string('name', 256).notNullable()
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
