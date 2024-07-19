0ff4cc3e2d7b4ea0a68e6299db0a46601a538ec0
leo <leonardoandrei41@gmail.com>
2024-07-19 17:23:42 -0300

    Add POST /login route to routes.ts

--- start/routes.ts
+++ start/routes.ts
@@ -9,5 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 import UsersController from '../app/controllers/users_controller.js'
+import AuthController from '../app/controllers/auth_controller.js'
 
 router.resource('/users', UsersController)
+router.post('/login', [AuthController, 'login'])
