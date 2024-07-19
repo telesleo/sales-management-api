4ddc67bc2558fdd654fe8cc868518ea2bd9e5723
leo <leonardoandrei41@gmail.com>
2024-07-19 17:28:43 -0300

    Add Customer model

---
+++ app/models/customer.ts
@@ -0,0 +1,19 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Customer extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare cpf: string
+
+  @column()
+  declare name: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
