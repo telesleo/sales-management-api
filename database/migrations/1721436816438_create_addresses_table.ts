e1098a50c0fa6ceb043b0c24d9d280c6f68ac77b
leo <leonardoandrei41@gmail.com>
2024-07-20 10:20:07 -0300

    Fix name of customers reference in create_addresses_table migration

--- database/migrations/1721436816438_create_addresses_table.ts
+++ database/migrations/1721436816438_create_addresses_table.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('customer_id').references('customer.id').unsigned().onDelete('CASCADE')
+      table.integer('customer_id').references('customers.id').unsigned().onDelete('CASCADE')
       table.string('country').notNullable()
       table.string('state').notNullable()
       table.string('city').notNullable()
