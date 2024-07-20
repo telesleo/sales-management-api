8d751b31a2d9b26a06e55bb8844f581ffaff84fc
leo <leonardoandrei41@gmail.com>
2024-07-20 11:28:14 -0300

    Fix method of PhoneNumbersController from sales_controller.ts to phone_numbers_controller.ts

--- app/controllers/sales_controller.ts
+++ app/controllers/sales_controller.ts
@@ -1,25 +1,15 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { saleValidator } from '../validators/sale.js'
-import Customer from '../models/customer.js'
+import Sale from '../models/sale.js'
 
 export default class SalesController {
-  /**
-   * Display a list of phone numbers of a client
-   */
-  async index({ params }: HttpContext) {
-    const { customerId } = params
-    const customer = await Customer.findOrFail(customerId)
-    await customer.load('phoneNumbers')
-    return customer.phoneNumbers
-  }
-
   /**
    * Add a new sale
    */
   async store({ request }: HttpContext) {
     const data = request.all()
     const { customerId, productId, amount } = await saleValidator.validate(data)
-    const customer = await Customer.findOrFail(customerId)
-    await customer.related('sales').attach({ [productId]: { amount } })
+    const sale = await Sale.create({ customerId, productId, amount })
+    return sale
   }
 }
