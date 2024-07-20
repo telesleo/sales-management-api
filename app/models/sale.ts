56119a69d690c3ed0d4a7b286e9e51efef7999f9
leo <leonardoandrei41@gmail.com>
2024-07-20 11:28:45 -0300

    Add Sale model

---
+++ app/models/sale.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Customer from './customer.js'
+import Product from './product.js'
+
+export default class Sale extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column({ serializeAs: null })
+  declare customerId: number
+
+  @column({ serializeAs: null })
+  declare productId: number
+
+  @column()
+  declare amount: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => Customer)
+  declare customer: BelongsTo<typeof Customer>
+
+  @belongsTo(() => Product)
+  declare product: BelongsTo<typeof Product>
+}
