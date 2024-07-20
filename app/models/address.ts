5fbd1af6af3427dc89fc92ebb323cc0126293326
leo <leonardoandrei41@gmail.com>
2024-07-19 21:58:47 -0300

    Add Address model

---
+++ app/models/address.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Address extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column({ serializeAs: null })
+  declare customerId: number
+
+  @column()
+  declare country: string
+
+  @column()
+  declare state: string
+
+  @column()
+  declare city: string
+
+  @column()
+  declare street: string
+
+  @column()
+  declare number: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
