93cb4c4596c98f9a9febde075fabb71f43d4c893
leo <leonardoandrei41@gmail.com>
2024-07-19 21:07:36 -0300

    Implement GET /products route for displaying a list of products

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -4,6 +4,14 @@ import Product from '../models/product.js'
 
 export default class ProductsController {
   /**
+   * Display a list of products
+   */
+  async index({}: HttpContext) {
+    const products = await Product.query().orderBy('name')
+    return products
+  }
+
+  /**
    * Add a new product
    */
   async store({ request }: HttpContext) {
