e9b83e1af083223735394de980313cd2b3f4539b
leo <leonardoandrei41@gmail.com>
2024-07-19 21:59:06 -0300

    Add address validator

---
+++ app/validators/address.ts
@@ -0,0 +1,21 @@
+import vine from '@vinejs/vine'
+
+const validatorBase = {
+  country: vine.string().trim(),
+  state: vine.string().trim(),
+  city: vine.string().trim(),
+  street: vine.string().trim(),
+  number: vine.number().positive(),
+}
+
+export const addressValidator = vine.compile(vine.object(validatorBase))
+
+export const optionalAddressValidator = vine.compile(
+  vine.object({
+    country: validatorBase.country.optional(),
+    state: validatorBase.state.optional(),
+    city: validatorBase.city.optional(),
+    street: validatorBase.street.optional(),
+    number: validatorBase.number.optional(),
+  })
+)
