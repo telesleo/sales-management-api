20c2ef259340b9484bfb3e9f387da8f31dd8f437
leo <leonardoandrei41@gmail.com>
2024-07-19 21:15:44 -0300

    Implement PUT /products route for updating a product

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -1,5 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import { productValidator } from '../validators/product.js'
+import { productValidator, productValidatorOptional } from '../validators/product.js'
 import Product from '../models/product.js'
 
 export default class ProductsController {
@@ -17,6 +17,20 @@ export default class ProductsController {
   async show({ params }: HttpContext) {
     const { id } = params
     const product = await Product.findOrFail(id)
+    return product
+  }
+
+  /**
+   * Update a product
+   */
+  async update({ request, params }: HttpContext) {
+    const { id } = params
+    const product = await Product.findOrFail(id)
+
+    const data = await productValidatorOptional.validate(request.all())
+    product.merge(data)
+    await product.save()
+
     return product
   }
 
