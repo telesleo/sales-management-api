c2204aa12156bc5fb6232d17c05a08546d45dd9f
leo <leonardoandrei41@gmail.com>
2024-07-20 13:19:15 -0300

    Modify address validator to make customerId optional

--- app/validators/address.ts
+++ app/validators/address.ts
@@ -13,6 +13,7 @@ export const addressValidator = vine.compile(vine.object(validatorBase))
 
 export const addressValidatorOptional = vine.compile(
   vine.object({
+    customerId: validatorBase.customerId.optional(),
     country: validatorBase.country.optional(),
     state: validatorBase.state.optional(),
     city: validatorBase.city.optional(),
