3d2066384b2d22b06988a72c0825ffeb6dacc04c
leo <leonardoandrei41@gmail.com>
2024-07-21 17:00:33 -0300

    Modify SalesController.store to store the unitPrice of the product

--- app/controllers/sales_controller.ts
+++ app/controllers/sales_controller.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { saleValidator } from '../validators/sale.js'
 import Sale from '../models/sale.js'
+import Product from '../models/product.js'
 
 export default class SalesController {
   /**
@@ -9,7 +10,9 @@ export default class SalesController {
   async store({ request }: HttpContext) {
     const data = request.all()
     const { customerId, productId, amount } = await saleValidator.validate(data)
-    const sale = await Sale.create({ customerId, productId, amount })
+    const product = await Product.findOrFail(productId)
+    const unitPrice = product.price
+    const sale = await Sale.create({ customerId, productId, amount, unitPrice })
     return sale
   }
 }
